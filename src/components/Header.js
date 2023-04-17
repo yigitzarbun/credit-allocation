79e8a6d0d5d7876e2ce561eb11fb15fdac375805
Yigit Zarbun <yzarbun@gmail.com>
2023-04-17 20:53:59 +0300

    merge test2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <div className="flex justify-between">
+      <p>test</p>
       <h1>Bank</h1>
       <nav className="flex justify-between w-1/4">
         <NavLink>Dashboard</NavLink>
