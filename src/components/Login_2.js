7bdb788b16302966a2cf923ace91f14d6b68d3e7
malkochilal <119036086+malkochilal@users.noreply.github.com>
2023-04-20 13:46:59 +0300

    login_2

--- src/components/Login_2.js
+++ src/components/Login_2.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import './Login.css';
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
     const [userEmail, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-
+    const { reset } = useForm();
     const handleSubmit = (e) => {
         e.preventDefault();
         if (userEmail === 'example' && password === 'password') {
@@ -16,47 +17,66 @@ const Login = () => {
             setError('Invalid username or password');
         }
     };
+    const handleLogin = (data) => {
+        reset();
+    };
+    const handleClearForm = () => {
+        reset();
+    };
 
     return (
         <body>
-            <div className="bg-orange-300">
+            <div className="bg-orange-300 form-box">
                 <h2 className="font-bold text-4xl text-center">Log In</h2> <br>
                 </br>
-                <form onSubmit={handleSubmit}
-                    className='loginForm flex flex-col mt-4"'>
+                <form
+                    onSubmit={handleSubmit(handleLogin)}
+                    className='loginForm flex flex-col mt-4 items-center'>
+                    <div classname="flex flex-col">
+                        <div className="form-group text-center">
+                            <label htmlFor="email" >Email:</label>
+                            <input
+                                type="text"
+                                placeholder='e.g. user@email.com'
+                                value={userEmail}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group text-center">
+                            <label>Password:</label>
+                            <input
+                                type="password"
 
-                    <div className="form-group text-center">
-                        <label htmlFor="email" >Email:</label>
-                        <input
-                            type="text"
-                            placeholder='e.g. user@email.com'
-                            value={userEmail}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
                     </div>
-                    <div className="form-group text-center">
-                        <label>Password:</label>
-                        <input
-                            type="password"
+                    <div>
+                        <button className="mt-4 mr-2   font-bold border-2 w-1/2 cursor-pointer border-green-500 rounded-md hover:bg-amber-800 hover:text-white p-2 py-px"
+                            type="submit">Login
 
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+
+
+                        </button>
+                        <button
+                            onClick={handleClearForm}
+                            className="font-bold mt-4 ml-2 w-1/2 border-2 border-red-500 rounded-md hover:bg-red-500 hover:text-white p-2 text-center"
+                        >
+                            <p>Clear</p>
+                        </button>
+
                     </div>
-                    <button className="mt-4 mr-2   font-bold border-2 w-1/2 cursor-pointer border-green-500 rounded-md hover:bg-amber-800 hover:text-white p-2 "
-                        type="submit">Login
-
-
-                    </button>
+                    <Link to="/register">
+                        <p className="mt-8">
+                            Don't have an account yet?
+                            <span className="text-blue-400 font-bold ml-2">Register</span>
+                        </p>
+                    </Link>
 
                     {error && <div className="error-message text-center">{error}</div>}
                 </form>
-                <Link to="/register">
-                    <p className="mt-8 underline-offset-2 text-center">
-                        Don't have an account yet?
-                        <span className="text-blue-400 font-bold ml-2">Register</span>
-                    </p>
-                </Link>
+
 
             </div >
         </body>
